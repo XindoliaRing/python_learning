0d711692567766e78090beab719a4d699382ed0c
Xindolia_Ring <57091352+XindoliaRing@users.noreply.github.com>
2021-11-22 16:16:53 +0800

    First Word - timetest
    complete running time with 4 kinds of functions -> first_word_1/2/3/4

--- First Word (simplified).py
+++ First Word (simplified).py
@@ -4,25 +4,59 @@ Created on Mon Nov 22 15:54:13 2021
 
 @author: Ring
 
-title: First Word
+title: First Word - timetest
 
 You are given a string and you have to find its first word.
+timetest
 """
+from timeit import timeit as t
 
-from string import punctuation, whitespace
-def first_word(str):
-    to_strip = punctuation + whitespace
-    return str.lstrip(to_strip).split(' ', 1)[0].rstrip(to_strip)
+def first_word(text):
+    index = text.find(" ")
+    return text[:index] if index != -1 else text
 
 
-if __name__ == '__main__':
-    print("Example:")
-    print(first_word("Hello world"))
-    print(first_word("a word"))
-    print(first_word("hi"))
+def first_word_1(text):
+    return text.split(" ")[0]
+
+print(t('first_word_1(x)', setup='x = "asdf we"*10', number=10000, globals=globals()))       #  ~11.7 ms
+print(t('first_word_1(x)', setup='x = "asdfawe"*10', number=10000, globals=globals()))       #  ~6.1 ms
+print(t('first_word_1(x)', setup='x = "asdf we"*100000', number=10000, globals=globals()))   #  ~90928.2 ms
+print(t('first_word_1(x)', setup='x = "asdfawe"*100000', number=10000, globals=globals()))   #  ~5562.9 ms
+
+
+def first_word_2(text):
+    index = text.find(" ")
+    return text[:index] if index != -1 else text
     
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert first_word("Hello world") == "Hello"
-    assert first_word("a word") == "a"
-    assert first_word("hi") == "hi"
-    #print("Coding complete? Click 'Check' to earn cool rewards!")
+print(t('first_word_2(x)', setup='x = "asdf we"*10', number=10000, globals=globals()))       #  ~6.3 ms
+print(t('first_word_2(x)', setup='x = "asdfawe"*10', number=10000, globals=globals()))       #  ~4.7 ms
+print(t('first_word_2(x)', setup='x = "asdf we"*100000', number=10000, globals=globals()))   #  ~7.0 ms
+print(t('first_word_2(x)', setup='x = "asdfawe"*100000', number=10000, globals=globals()))   #  ~2108.4 ms
+
+
+def first_word_3(text):
+    try:
+        index = text.index(" ")
+        return text[:index]
+    except ValueError:
+        return text
+
+print(t('first_word_3(x)', setup='x = "asdf we"*10', number=10000, globals=globals()))       #  ~5.8 ms
+print(t('first_word_3(x)', setup='x = "asdfawe"*10', number=10000, globals=globals()))       #  ~8.5 ms
+print(t('first_word_3(x)', setup='x = "asdf we"*100000', number=10000, globals=globals()))   #  ~5.8 ms
+print(t('first_word_3(x)', setup='x = "asdfawe"*100000', number=10000, globals=globals()))   #  ~2005.8 ms
+
+
+def first_word_4(text):
+    index = -1
+    for pos, letter in enumerate(text):
+        if letter == " ":
+            index = pos
+            break
+    return text[:index] if index != -1 else text
+    
+print(t('first_word_4(x)', setup='x = "asdf we"*10', number=10000, globals=globals()))       #  ~13.1 ms
+print(t('first_word_4(x)', setup='x = "asdfawe"*10', number=10000, globals=globals()))       #  ~71.1 ms
+print(t('first_word_4(x)', setup='x = "asdf we"*100000', number=10000, globals=globals()))   #  ~13.1 ms
+print(t('first_word_4(x)', setup='x = "asdfawe"*100000', number=10000, globals=globals()))   #  ~788793.7 ms
